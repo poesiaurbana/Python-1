2c50be3b91f210f6b2e2389bec3f6e5b2747ed15
jPablo <juapabgonzalez@utp.edu.co>
2024-07-07 22:03:44 -0500

    mno

---
+++ Learn_python/POO/HerMultiple.py
@@ -0,0 +1,29 @@
+class telefono:
+    def __init__(self):
+        pass
+    def encender(self):
+        print("El telefono esta encendido")
+
+class camara:
+    def __init__(self):
+        pass
+    def photo(self):
+        print("Tomando photo")
+    def video(self):
+        print("El telefono esta tomando video")
+
+class linea:
+    def __init__(self):
+        pass
+    def llamada(self):
+        print("Llamando...")
+    def colgar(self):
+        print("Colgado...")
+
+class iphone(telefono,camara,linea):
+        def __del__(self):
+            print("El telefono ha finalizado la accion")
+
+
+cel1 = iphone()
+print(cel1.colgar())
